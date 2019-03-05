ea53d13ee6a59706f655ffe6d0c52bca9ff7285b
Ruslan Curbanov <ruslan.curbanov@uni-duesseldorf.de>
2019-03-05 13:50:55 +0100

    cleanup

--- src/main/java/science/atlarge/graphalytics/dxram/algorithms/bfs/interfaces/TraversalEdgeCallback.java
+++
@@ -1,8 +0,0 @@
-package science.atlarge.graphalytics.dxram.algorithms.bfs.interfaces;
-
-import science.atlarge.graphalytics.dxram.graph.data.Edge;
-
-public interface TraversalEdgeCallback {
-    // return false to terminate the traversal (because result found, error, ...), true to continue
-    boolean evaluateEdge(Edge p_edge, int p_depth);
-}
--- src/main/java/science/atlarge/graphalytics/dxram/algorithms/bfs/interfaces/TraversalVertexCallback.java
+++
@@ -1,8 +0,0 @@
-package science.atlarge.graphalytics.dxram.algorithms.bfs.interfaces;
-
-import science.atlarge.graphalytics.dxram.graph.data.Vertex;
-
-public interface TraversalVertexCallback {
-    // return false to terminate the traversal (because result found, error, ...), true to continue
-    boolean evaluateVertex(Vertex p_vertex, int p_depth);
-}
